cd8f00448549d77eb8a11bb1f556dc827d86707a
Khanyicou <preciouskhanyisa1@gmail.com>
2024-02-23 16:01:59 -0800

    ou

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in /tmp
+# manifest to create file in tmp directory
 
 file { '/tmp/school':
   content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
- ensure  => file,
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #!/usr/bin/pup
-# an especific version of flask install (2.1.0)
+# Install an especific version of flask (2.1.0)
+# puppet declarative script to grab puppet-lint
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
